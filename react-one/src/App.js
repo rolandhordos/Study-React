b629928e6d9e1538cd34c7290296a10a39b2e357
Roland Hordos <rolandhordos@me.com>
2018-01-06 17:19:23 -0800

    App passing with flow and jest.

--- react-one/src/App.js
+++ react-one/src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
 
-class App extends Component {
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+export default class App extends Component<{}> {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Study</h1>
+          <h3>with Redux Saga</h3>
         </header>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
-
-export default App;
